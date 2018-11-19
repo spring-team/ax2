39de1f08b740540f920ed43d02571f6753e6e74e
Rod Johnson <johnsonroda@gmail.com>
2018-11-19 14:01:33 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/a2/Ax2Application.java
@@ -0,0 +1,12 @@
+package com.a2;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ax2Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Ax2Application.class, args);
+	}
+
+}
